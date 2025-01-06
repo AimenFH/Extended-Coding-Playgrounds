0f1bf66b29405e211c2a4ae297df575ac3473865
Aimen <aimen.el-sayed@stud.fh-campuswien.ac.at>
2025-01-06 18:43:10 +0100

    action

--- angular-bear/src/app/app.component.spec.ts
+++ angular-bear/src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
+// src/app/app.component.spec.ts
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +17,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it("should have the 'angular-boilerplate' title", () => {
+  it(`should have as title 'angular-boilerplate'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-boilerplate');
@@ -24,6 +27,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-boilerplate');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-boilerplate app is running!');
   });
 });
--- angular-bear/src/app/app.component.ts
+++ angular-bear/src/app/app.component.ts
@@ -1,14 +1,11 @@
+// src/app/app.component.ts
 import { Component } from '@angular/core';
-import { NavigationComponent } from './navigation/navigation.component';
-import { CommentsComponent } from './comments/comments.component';
-import { FooterComponent } from './footer/footer.component';
-import { MainContentComponent } from '@app/main-content/main-content.component';
-import { HeaderComponent } from '@app/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  imports: [NavigationComponent, HeaderComponent, MainContentComponent, CommentsComponent, FooterComponent],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'angular-boilerplate';
+}
